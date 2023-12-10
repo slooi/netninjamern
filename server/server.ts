d103482d40d10d0b815a1febd0f1418eaffc151c
Samuel <s.looi2@outlook.com>
2023-12-11 01:00:14 +1300

    reformat

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,14 @@
-import express from "express"
-import { routerWorkouts } from "./routers/workouts"
-import mongoose from "mongoose"
-import { CONFIG } from "./config"
-
+import express from "express";
+import { routerWorkouts } from "./routers/workouts";
 
 // Create express app
-export const app = express()
+export const app = express();
 
 // MIDDLEWARE
-app.use((req,res,next)=>{
-	console.log("req.path req.method:",req.path,req.method)
-	next()
-})
+app.use((req, res, next) => {
+	console.log("req.path req.method:", req.path, req.method);
+	next();
+});
 
 // Routes
-app.use("/api",routerWorkouts)
+app.use("/api", routerWorkouts);

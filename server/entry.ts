c88f1147d6ac280f2af8b7d41212bffdd283ea54
Samuel <s.looi2@outlook.com>
2023-12-11 01:37:04 +1300

    add testing code

--- server/entry.ts
+++ server/entry.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { CONFIG } from "./config"
 import { app } from "./server"
+import { ModelWorkouts } from "./validatorsmodelstypes/Workout";
 
 
 (async function (){
@@ -8,7 +9,10 @@ import { app } from "./server"
 		// Connect to mongodb server
 		await mongoose.connect(CONFIG.MONGO_URL)
 		console.log("CONNECTED TO MONGODB");
+
+		// console.log(await ModelWorkouts.create({title:"test","reps":1,"load":2}))
 		
+
 		app.listen(CONFIG.PORT,()=>console.log("Listening on port "+CONFIG.PORT))
 	}catch(err){
 		throw new Error("ERROR: "+err)

6588ba817276c6d8039dce721abe4bb387e7ec60
s <s.looi2@outlook.com>
2023-12-12 22:26:41 +1300

    Add types to express json

--- server/packageOverrides.ts
+++ server/packageOverrides.ts
@@ -1,6 +1,7 @@
-// declare module "express" {
-// 	interface Response {
-// 		json: (body: DataOrError) => this;
-// 	}
-// }
-// type DataOrError = { error: any; data?: never } | { error?: never; data: any }
+import express from "express"
+declare module "express" {
+	interface Response {
+		json: (body: DataOrError) => this;
+	}
+}
+type DataOrError = { error: any; data?: never } | { error?: never; data: any }

f14355d746963668afec3a2982c21a269db81f99
Samuel <s.looi2@outlook.com>
2023-12-12 16:30:10 +1300

    add error handling

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { routerWorkouts } from "./routers/workouts";
 
 // Create express app
@@ -10,10 +10,26 @@ app.use(express.json())
 
 app.use((req, res, next) => {
 	console.log("req.path req.method:", req.path, req.method);
-	console.log("req.body:",req.body)
+	console.log("req.body:", req.body)
 	next();
 });
 
-
 // Routes
 app.use("/api", routerWorkouts);
+
+// type DataOrError = { error: any; data?: never } | { error?: never; data: any }
+// interface DataOrErrorResponse extends Response {
+// 	json: (body: DataOrError) => this
+// }
+
+// interface Express
+
+
+// THIS MUST BE BELOW ALL OTHER MIDDLEWARE INCLUDING MIDDLEWARE ROUTES (IDK ABOUT just NORMAL ROUTES THOUGH)
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+	console.log("####################### ERROR #######################")
+	console.log("req.path req.method:", req.path, req.method);
+	console.log("req.body:", req.body)
+	console.log(error)
+	res.json({ error: error })
+})

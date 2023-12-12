111980698707eb66bce8860c8229274deb68855a
Samuel <s.looi2@outlook.com>
2023-12-12 17:35:58 +1300

    mv file and add notes

---
+++ server/utils.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express"
+
+// Wrap your async controllers with this! This automatically calls the `next` function when the 
+// function argument throws an errors allowing you to have a centralized location for handling errors 
+export const asyncNextCaller = <T extends (...any: any) => any>(fn: T) => (
+	function asyncUtilWrap(req: Request, res: Response, next: NextFunction, ...args: any) {
+		const fnReturn = fn(req, res, next, ...args)
+		return Promise.resolve(fnReturn).catch(next)
+	}
+)

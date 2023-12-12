ecb0e8f44a04a764d2f0226aeccc357e99351f6b
Samuel <s.looi2@outlook.com>
2023-12-12 16:30:36 +1300

    Add basic error format

---
+++ server/error.ts
@@ -0,0 +1,25 @@
+// class APIError {
+// 	constructor(code, message) {
+// 		this.code = code
+// 		this.message = message
+// 	}
+// 	static badRequest(msg) {
+// 		return new APIError(400, msg)
+// 	}
+// 	static internal(msg) {
+// 		return new APIError(500, msg)
+// 	}
+// }
+
+// function apiErrorHandler(err, req, res, next) {
+// 	// in prod don't use console.log or prod, it's not async
+// 	// use wintons
+// 	console.error(err)
+
+
+// 	if (err instanceof ApiError) {
+// 		res.status(err.code).json()
+// 		return;
+// 	}
+// 	res.status(500).json()
+// }

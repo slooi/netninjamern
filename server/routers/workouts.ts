a0f89d989b8d16630ef48d709ee98f6c4062eea2
Samuel <s.looi2@outlook.com>
2023-12-12 17:36:28 +1300

    Add basic routes

--- server/routers/workouts.ts
+++ server/routers/workouts.ts
@@ -1,64 +1,63 @@
 import express, { NextFunction, Request, Response } from "express";
 import { ModelWorkouts, ZodSchemaWorkout } from "../validatorsmodelstypes/Workouts";
-import { z } from "zod";
+import { asyncNextCaller } from "../utils";
 
 const router = express.Router()
 
+
+
 // GET all
-router.get(`/`, (req, res) => {
-	res.status(200).json({ msg: "GET all workouts" })
+router.get(`/`, async (req, res) => {
+	res.status(200).json(await ModelWorkouts.find())
 })
+
+
 
 // GET id
-router.get(`/:id`, (req, res) => {
+router.get(`/:id`, asyncNextCaller(async (req, res) => {
 	const { id } = req.params
+	console.log("req.params \t", req.params)
+
+	console.log('await ModelWorkouts.findById(id) \t', await ModelWorkouts.findById(id))
+
 	res.status(200).json({ msg: `GET workout: ${id}` })
-})
+}))
 
 
-function createValidateMiddleware<T extends z.ZodRawShape>(zodSchema: z.ZodObject<T>) {
-
-	return (req: Request, res: Response, next: NextFunction) => {
-		try {
-			// Validate the request body against Zod schema
-			zodSchema.parse(req.body);
-			next();
-		} catch (error) {
-			res.status(400).json({ error: error });
-		}
-	};
-}
 
 // POST
-router.post(`/`, createValidateMiddleware(ZodSchemaWorkout), async (req, res) => {
-	try {
-		console.log(req.body)
-		const workout = await ModelWorkouts.create({
-			load: 1000,
-			reps: 1000,
-			title: "naiwa"
-		})
-		console.log("wah")
-		console.log(await ModelWorkouts.find())
-		console.log(1231231231231231231312)
-		res.status(200).json({ msg: workout })
-	} catch (err) {
-		console.log(1231231231231231231312)
-		res.status(400).json({ msg: "Error creating workouts" })
-	}
-})
+router.post(`/`, asyncNextCaller(async (req, res, next) => {
+	const workout = await ModelWorkouts.create({ ...ZodSchemaWorkout.parse(req.body) })
+
+	console.log("await ModelWorkouts.find() \t", await ModelWorkouts.find())
+
+	res.status(200).json({ msg: workout })
+}))
+
+
 
 // DELETE
-router.delete("/:id", (req, res) => {
+router.delete("/:id", asyncNextCaller(async (req, res) => {
+
 	const { id } = req.params
+	console.log("await ModelWorkouts.deleteOne(id) \t", await ModelWorkouts.deleteOne(id))
 	res.status(200).json({ msg: "DELETE workout: " + id })
-})
+}))
+
+
 
 // PATCH
-router.patch("/:id", (req, res) => {
+router.patch("/:id", asyncNextCaller(async (req, res) => {
 	const { id } = req.params
+
+	// READ worker
+	const workout = await ModelWorkouts.find(id)
+
+	// UPDATE worker
+	// workout.
+
 	res.status(200).json({ msg: "PATCH workout: " + id })
-})
+}))
 
 
 

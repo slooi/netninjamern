db7fc6e1217f36d7568b0989a9fc9505d87d1b07
Samuel <s.looi2@outlook.com>
2023-12-14 05:26:39 +1300

    Comment out testing code

--- server/entry.ts
+++ server/entry.ts
@@ -19,28 +19,23 @@ import { ModelWorkouts } from "./validatorsmodelstypes/Workouts";
 		});
 
 
-		const userSchema = new mongoose.Schema({
-			name: { type: String, required: true },
-			age: { type: Number, min: 13, max: 90 }
-		});
+		// const userSchema = new mongoose.Schema({
+		// 	name: { type: String, required: true },
+		// 	age: { type: Number, min: 13, max: 90 }
+		// });
+		// const User = mongoose.model('User', userSchema);
+		// const hafez = new User({ name: 'Hafez', age: 24 });
+		// // hafez.save()
+		// async function run() {
+		// 	try {
+		// 		const a = await User.findByIdAndUpdate("6579d9d8212c13cec7f0907c", { age: 10 });
+		// 		console.log("a", a)
 
-
-		const User = mongoose.model('User', userSchema);
-
-		const hafez = new User({ name: 'Hafez', age: 24 });
-		// hafez.save()
-
-		async function run() {
-			try {
-				const a = await User.findByIdAndUpdate("6579d9d8212c13cec7f0907c", { age: 10 });
-				console.log("a", a)
-
-			} catch {
-				console.log("err happened")
-			}
-		}
-
-		await run()
+		// 	} catch {
+		// 		console.log("err happened")
+		// 	}
+		// }
+		// await run()
 
 
 		app.listen(CONFIG.PORT, () => console.log("Listening on port " + CONFIG.PORT))

0451b0cd54714549d2cdf850e044dfd4f77b92f3
Samuel <s.looi2@outlook.com>
2023-12-14 00:02:45 +1300

    Reformat

--- server/packageOverrides.ts
+++ server/packageOverrides.ts
@@ -1,7 +1,11 @@
+// ####################################################
+//		Ensure that all json responses are constrained to 3 types
+// ####################################################
 import express from "express"
+
 declare module "express" {
 	interface Response {
 		json: (body: DataOrError) => this;
 	}
 }
-type DataOrError = { error: any; data?: never } | { error?: never; data: any }
+type DataOrError = { data: any; error?: never; error500?: never; } | { data?: never; error: any; error500?: never; } | { data?: never; error?: never; error500?: any; }

6dbbe012401dfcf878d68e730170960091f0359a
Samuel <s.looi2@outlook.com>
2023-12-14 16:46:38 +1300

    Rename Workouts.ts to workout.ts

--- server/entry.ts
+++ server/entry.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { CONFIG } from "./config/config"
 import { app } from "./server"
-import { ModelWorkouts } from "./validatorsmodelstypes/Workouts";
+import { ModelWorkouts } from "./validatorsmodelstypes/workouts";
 
 
 (async function () {

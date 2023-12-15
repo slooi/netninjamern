5dc1213297bb198f820dd4bff896d6a10bcf6566
Samuel <s.looi2@outlook.com>
2023-12-16 05:09:29 +1300

    test strict zod

--- server/validatorsmodelstypes/workouts.ts
+++ server/validatorsmodelstypes/workouts.ts
@@ -8,6 +8,16 @@ export const ZodSchemaWorkout = z.object({
 	load: z.number()
 })
 
+// .strict()
+// try {
+// 	const validatedData = ZodSchemaWorkout.parse({ title: "hi", reps: 1, load: 2, extraField: "ahaha" });
+// 	console.log("Validation successful:");
+// } catch (err) {
+// 	console.error("Validation failed:");
+// }
+
+
+
 // Type
 export type Workout = z.infer<typeof ZodSchemaWorkout>
 
@@ -26,6 +36,15 @@ const SchemaWorkout = new mongoose.Schema<Workout>({
 		required: true
 	}
 }, { timestamps: true, strict: "throw" })
+
+
+
+
+
+
+
+
+
 
 // Model
 export const ModelWorkouts = mongoose.model("workouts", SchemaWorkout)

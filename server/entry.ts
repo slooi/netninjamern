3f64504a7eeb327fba8819020be1666b9e2ec90a
s <s.looi2@outlook.com>
2023-12-18 19:57:51 +1300

    Remove comment

--- server/entry.ts
+++ server/entry.ts
@@ -19,26 +19,7 @@ import { ModelWorkouts } from "./validatorsmodelstypes/workouts";
 			schema.pre('updateOne', function () { this.setOptions({ runValidators: true }) });
 		});
 
-
-		// const userSchema = new mongoose.Schema({
-		// 	name: { type: String, required: true },
-		// 	age: { type: Number, min: 13, max: 90 }
-		// });
-		// const User = mongoose.model('User', userSchema);
-		// const hafez = new User({ name: 'Hafez', age: 24 });
-		// // hafez.save()
-		// async function run() {
-		// 	try {
-		// 		const a = await User.findByIdAndUpdate("6579d9d8212c13cec7f0907c", { age: 10 });
-		// 		console.log("a", a)
-
-		// 	} catch {
-		// 		console.log("err happened")
-		// 	}
-		// }
-		// await run()
-
-
+		// Listen
 		app.listen(CONFIG.PORT, () => console.log("Listening on port " + CONFIG.PORT))
 	} catch (err) {
 		throw new Error("ERROR: " + err)
